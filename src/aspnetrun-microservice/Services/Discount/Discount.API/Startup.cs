106388f6871c2bb66e27ed00c9681b43fae76f43
KyawHtay <Kyaw.Htay@gmail.com>
2022-04-08 00:53:21 +0100

    Add discount API yml files.

--- src/aspnetrun-microservice/Services/Discount/Discount.API/Startup.cs
+++ src/aspnetrun-microservice/Services/Discount/Discount.API/Startup.cs
@@ -30,7 +30,7 @@ namespace Discount.API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
             });
         }
 
@@ -41,7 +41,7 @@ namespace Discount.API
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Discount.API v1"));
             }
             else
             {
