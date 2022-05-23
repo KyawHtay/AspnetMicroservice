852d49fd1d9f82d4cd3f58fa7cc1fa583b2b18fb
KyawHtay <Kyaw.Htay@gmail.com>
2022-05-24 00:07:11 +0100

    Add Clean Architecture and CQRS Implementation

---
+++ src/aspnetrun-microservice/Services/Ordering/Ordering.Application/Features/Orders/Quaries/GetOrderList/GetOrdersListQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Quaries.GetOrderList
+{
+    public class GetOrdersListQueryHandler : IRequestHandler<GetOrdersListQuery, List<OrdersVm>>
+    {
+        private readonly IOrderRepository _orderyRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersListQueryHandler(IOrderRepository orderyRepository, IMapper mapper)
+        {
+            _orderyRepository = orderyRepository ?? throw new ArgumentNullException(nameof(orderyRepository));
+            this._mapper = mapper ?? throw new ArgumentNullException(nameof(mapper)); ;
+        }
+
+        public async Task<List<OrdersVm>> Handle(GetOrdersListQuery request, CancellationToken cancellationToken)
+        {
+           var orderList= await _orderyRepository.GetOrdersByUserName(request.UserName);
+            return _mapper.Map<List<OrdersVm>>(orderList);
+        }
+    }
+}
